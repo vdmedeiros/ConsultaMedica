83b175e0cf76f73b3a3d9e3e165777d6b33effe8
vdmedeiros <vini.dmedeiros@gmail.com>
2020-08-06 17:05:26 -0300

    publish

--- src/ConsultaMedica.Core/Communication/Mediator/IMediatorHandler.cs
+++ src/ConsultaMedica.Core/Communication/Mediator/IMediatorHandler.cs
@@ -12,6 +12,6 @@ namespace ConsultaMedica.Core.Communication.Mediator
         Task PublicarEvento<T>(T evento) where T : Event;
         Task<bool> EnviarComando<T>(T comando) where T : Command;
         Task PublicarNotificacao<T>(T notificacao) where T : DomainNotification;
-        //Task PublicarDomainEvent<T>(T notificacao) where T : DomainEvent;
+        
     }
 }
